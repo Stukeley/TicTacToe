8a7d3c07e857e0617d1b4d159f5c21563784a7e0
Rafał Klinowski <klinowskirafal@gmail.com>
2019-04-16 12:47:25 +0200

    Add routing to the registration page

--- TicTacToe/Startup.cs
+++ TicTacToe/Startup.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using TicTacToe.Extensions;
+using TicTacToe.Models;
 using TicTacToe.Services;
 
 namespace TicTacToe
@@ -14,6 +18,7 @@ namespace TicTacToe
 		{
 			services.AddMvc();
 			services.AddSingleton<IUserService, UserService>();
+			services.AddRouting();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,12 +36,32 @@ namespace TicTacToe
 
 			app.UseStaticFiles();
 
+			var routeBuilder = new RouteBuilder(app);
+			routeBuilder.MapGet("CreateUser", context =>
+			{
+				var firstName = context.Request.Query["firstName"];
+				var lastName = context.Request.Query["lastName"];
+				var email = context.Request.Query["email"];
+				var password = context.Request.Query["password"];
+				var userService = context.RequestServices.GetService<IUserService>();
+				userService.RegisterUser(new UserModel { FirstName = firstName, LastName = lastName, Email = email, Password = password });
+				return context.Response.WriteAsync($"Użytkownik {firstName} {lastName} został pomyślnie utworzony.");
+			});
+
+			var newUserRoutes = routeBuilder.Build();
+			app.UseRouter(newUserRoutes);
+
+			var options = new RewriteOptions().AddRewrite("NewUser", "/UserRegistration/Index", false);
+			app.UseRewriter(options);
+
 			app.UseCommuncationMiddleware();
 
 			app.UseMvc(routes =>
 			{
 				routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
 			});
+
+			app.UseStatusCodePages("text/plain", "Błąd HTTP - kod odpowiedzi: {0}");
 		}
 	}
 }
