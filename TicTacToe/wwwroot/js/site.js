37cbf2b2e0a9fe1025dcd9ec25869eaa54dac769
Rafał Klinowski <klinowskirafal@gmail.com>
2019-04-17 19:17:01 +0200

    Better optimization using Bundler & Minifier

---
+++ TicTacToe/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+let interval;
+
+function EmailConfirmation(email) {
+    interval = setInterval(() => {
+        CheckEmailConfirmationStatus(email);
+    }, 5000);
+}
+function CheckEmailConfirmationStatus(email) {
+    $.get("/CheckEmailConfirmationStatus?email=" + email, function (data) {
+        if (data === "OK") {
+            if (interval !== null) {
+                clearInterval(interval);
+            }
+            window.location.href = "/GameInvitation?email=" + email;
+        }
+    })
+}
